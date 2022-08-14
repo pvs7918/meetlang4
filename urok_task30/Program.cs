4ca27e35bf4f200c68de24174f237d57b85cac8b
Pavel <p_v_s@mail.ru>
2022-08-14 15:44:34 +0700

    Добавил задачи до 30-й

---
+++ urok_task30/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями
+и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+
+void FillArray(int []arr)
+{
+    Random rnd = new Random();
+    
+    for (int i=0;i<arr.Length;i++)
+    {
+        arr[i]=rnd.Next(0,2);
+    }
+}
+
+void ViewArray(int []arr)
+{
+    string str = string.Empty;
+    Console.WriteLine("Значения элементов массива: ");
+    for (int i=0;i<arr.Length;i++)
+    {
+        if (i==0)
+        {
+            str+=arr[i];
+        }
+        else
+        {
+            str+=$", {arr[i]}";
+        }
+    }
+    Console.WriteLine(str);
+}
+
+int size = 8;
+int []arr = new int[size];
+FillArray(arr);
+ViewArray(arr);
