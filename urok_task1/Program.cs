927f63dca9f3de90c2b6cf50f52cdea3eabc9b83
Pavel <p_v_s@mail.ru>
2022-08-14 14:40:29 +0700

    первый коммит

---
+++ urok_task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывести сумму от 1 до N
+
+int GetSumFrom1ToN(int N)
+{
+    int sum = 0;
+    for (int i=1;i<=N;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+int sum = 0;
+
+
+
+Console.Write($"Сумма от 1 до N равна: {GetSumFrom1ToN(N)}");
---
+++ urok_task26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая вычисляет количество цифр в числе
+
+int CalcNumbersDigitCount(int N)
+{
+    int cnt = 0;
+    int remainder=N;
+    while (remainder>0)
+    {
+        if (remainder<10)
+        {
+            cnt+=1;
+            break;
+        }
+        else
+        {
+            remainder/=10;
+            cnt+=1;
+        }
+    }
+
+    return cnt;
+}
+
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Количество цифр в числе {N} равно: {CalcNumbersDigitCount(N)}");
